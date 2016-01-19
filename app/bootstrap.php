173a2adb6ea78106ced39d7a5064b29410e43dba
Anon <lexdss@gmail.com>
2016-01-19 20:17:33 +0200

    v1.3

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,15 +1,6 @@
 <?php
 session_start();
-//Папки из которых будут подключаться файлы
-/*set_include_path(
-				 PATH_SEPARATOR.'app/model'
-				.PATH_SEPARATOR.'app/view'
-				.PATH_SEPARATOR.'app/controller'
-				.PATH_SEPARATOR.'app/model/DataMapper'
-				.PATH_SEPARATOR.'app/model/DomainObjects'
-				.PATH_SEPARATOR.'app/model/ServiceObjects'
-				.PATH_SEPARATOR.'app/model/Validate'
-				);*/
+
 //Подключение конфига
 include 'config.php';
 
