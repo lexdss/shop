85da165b7a59f346b7805d459446489bd3c7760c
Anon <lexdss@gmail.com>
2016-02-06 16:40:22 +0200

    some fix

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,12 +1,16 @@
 <?php
 session_start();
 
-//Подключение конфига
+//Define dirs
+define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT']);
+define('APP_DIR', ROOT_DIR . '/app/');
+define('VIEW_DIR', APP_DIR . '/view/');
+
 include 'config.php';
 
 $db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8;", DB_USER, DB_PASS);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-//Автозагрузка классов
+//Autoload
 include 'autoloader.php';
 spl_autoload_register('loadClass');
--- app/config.php
+++ app/config.php
@@ -4,6 +4,3 @@ const DB_HOST = 'localhost';
 const DB_NAME = 'db_test';
 const DB_USER = 'root';
 const DB_PASS = 'root';
-
-const VIEW_DIR = __DIR__ . '/view/'; //Файлы вида
-const APP_DIR = __DIR__;
