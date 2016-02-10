a48277416dabcc4c51ad9b3ef9add5379e81c771
Anon <lexdss@gmail.com>
2016-02-10 20:26:13 +0200

    routing upd

--- app/controller/FrontController.php
+++ app/controller/FrontController.php
@@ -1,49 +1,64 @@
 <?php
 
 class FrontController{
-	private $controller;
-	private $action;
+
 	private $db;
+
 	//Определение контроллера и экшена
 	function __construct(PDO $db){
 
 		$this->db = $db;
 
-		//Определяем контроллер и экшен
-		$request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-		$request_arr = explode('/', trim($request, '/'));
-		if(!empty($request_arr[0])){
-			$this->controller = ucfirst(strtolower($request_arr[0])).'Controller';
-		}else{
-			$this->controller = 'IndexController';
-		}
-
-		if(!empty($request_arr[1])){
-			$this->action = strtolower($request_arr[1]).'Action';
-		}else{
-			$this->action = 'indexAction';
-		}
-
-		if(count($request_arr) > 2){
-			$this->page404();
-		}
 	}
 	
 	//Исполнение экшена в контроллере
 	public function route(){
-		if(class_exists($this->controller)){
-			$controller = new $this->controller($this->db);
-			$action = $this->action;
-			$controller->$action();
+		$request = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+		$request_arr = explode('/', $request);
+		
+		if($request == ''){
+			$controller = new IndexController($this->db);
+			$controller->indexAction();
+		}elseif($request == 'basket'){
+			$controller = new BasketController($this->db);
+			$controller->indexAction();
+		}elseif($request == 'auth'){
+			$controller = new AuthController($this->db);
+			$controller->indexAction();
+		}elseif($request == 'reg'){
+			$controller = new RegController($this->db);
+			$controller->indexAction();
+		}elseif($request == 'admin'){
+			$controller = new AdminController($this->db);
+			$controller->indexAction();
+		}elseif($request == 'admin/additem'){
+			$controller = new AdminController($this->db);
+			$controller->additemAction();
+		}elseif($request == 'admin/category'){
+			$controller = new AdminController($this->db);
+			$controller->categoryAction();
+		}elseif($request == 'admin/catalog'){
+			$controller = new AdminController($this->db);
+			$controller->catalogAction();
+		}elseif($request == 'admin/orders'){
+			$controller = new AdminController($this->db);
+			$controller->ordersAction();
+		}elseif($request = 'cat/' . $request_arr[1]){
+			$category_mapper = new CategoryMapper($this->db);
+			if(!$category_mapper->getCategoryFromCode($request_arr[1])){
+				$this->get404();
+			}else{
+				$controller = new CatController($this->db);
+				$controller->categoryAction($request_arr[1]);
+			}
 		}else{
-			$this->page404();
+			$this->get404();
 		}
 	}
 	
-	//404 страница
-	public static function page404(){
+	public static function get404(){
 		header('HTTP/1.1 404 Page Not Found');
-		include VIEW_DIR.'404page.tpl.php';
+		include VIEW_DIR . '404page.tpl.php';
 		exit;
 	}
 }
