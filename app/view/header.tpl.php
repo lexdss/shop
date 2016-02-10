1d3e77036baadba6c5fb66909e9f3c955eb7d6b4
Anon <lexdss@gmail.com>
2016-02-10 20:29:12 +0200

    small fix

--- app/view/header.tpl.php
+++ app/view/header.tpl.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 	<div class="header">
-		<div class="basket"><a href='/basket/'>Корзина:</a> <?=$this->basket_info['total_count'];?> товаров на сумму: <?=$this->basket_info['total_sum'];?> руб.</div>
+		<div class="basket"><a href='/basket'>Корзина:</a> <?=$this->basket_info['total_count'];?> товаров на сумму: <?=$this->basket_info['total_sum'];?> руб.</div>
 		<?php if(!$_SESSION['user_id']):?>
 		<div class="auth">
 			<form action='/auth' method='POST'>
@@ -13,11 +13,11 @@
 				<input type='submit' value='Вход' name='login'>
 				<span class="error"><?=$_SESSION['message'];?></span>
 			</form>
-			<a href='/reg/'>Регистрация</a>
+			<a href='/reg'>Регистрация</a>
 		</div>
 		<?php endif;?>
 		<?php if($_SESSION['user_role'] == 'admin'):?>
-			<a href='/admin/'>Панель управления</a>
+			<a href='/admin'>Панель управления</a>
 		<?php endif;?>
 		<?php if(isset($_SESSION['user_id'])):?>
 			<a href='/auth?act=logout'>Выход</a>
